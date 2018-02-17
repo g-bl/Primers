bc6c8fd8a051d58998085d631b918ef00941363f
g-bl <heyfromguillaume@gmail.com>
2018-02-17 16:41:29 +0100

    Searching for big squares first : 6 861 operations (best score is 2 896)

--- ArtOptimal/ArtOptimal.cs
+++ ArtOptimal/ArtOptimal.cs
@@ -22,7 +22,8 @@ namespace Primers
                 // Better use an array-of-arrays than a 2d array
                 // https://stackoverflow.com/questions/597720/what-are-the-differences-between-a-multidimensional-array-and-an-array-of-arrays
                 bool[][] painting;
-                
+                bool[][] alreadyFilledPixels;
+
                 int width = 0;
                 int height = 0;
 
@@ -39,37 +40,77 @@ namespace Primers
                     }
 
                     painting = new bool[height][];
+                    alreadyFilledPixels = new bool[height][];
 
                     for (int i = 0; i < height; i++)
                     {
                         painting[i] = new bool[width];
+                        alreadyFilledPixels[i] = new bool[width];
 
-                        for (int j = 0; j < width; j++)
+                        char[] charsOnTheLine = lines[i + 1].ToCharArray();
+
+                        for (int j = 0; j < charsOnTheLine.Length; j++)
                         {
-                            char[] chars = lines[i+1].ToCharArray();
-                            painting[i][j] = (chars[j].Equals(filledPixel));
+                            painting[i][j] = (charsOnTheLine[j].Equals(filledPixel));
+                            alreadyFilledPixels[i][j] = false;
                         }
                     }
 
                     // SOLVING
                     List<Operation> operations = new List<Operation>();
 
-                    // Worst solver only uses 1x1 squares
-                    for (int i = 0; i < painting.Length; i++)
+                    // Searching from big squares to little ones
+                    for (int squareSize = Math.Min(width, height); squareSize > 0; squareSize--)
                     {
-                        for (int j = 0; j < painting[i].Length; j++)
+                        System.Diagnostics.Debug.WriteLine("squareSize: " + squareSize);
+
+                        for (int squareOriginY = 0; squareOriginY <= (height - squareSize); squareOriginY++)
                         {
-                            if (painting[i][j])
-                                operations.Add(new Fill()
+                            for (int squareOriginX = 0; squareOriginX <= (width - squareSize); squareOriginX++)
+                            {
+                                // Validating the square
+                                bool squareIsValid = true;
+
+                                for (int y = squareOriginY; y < (squareOriginY + squareSize); y++)
                                 {
-                                    x = i,
-                                    y = j,
-                                    size = 1
-                                });
+                                    for (int x = squareOriginX; x < (squareOriginX + squareSize); x++)
+                                    {
+                                        // The square must exclude any blank or already filled pixels
+                                        if (!painting[y][x] || alreadyFilledPixels[y][x])
+                                        {
+                                            squareIsValid = false;
+                                            break;
+                                        }
+                                    }
+                                    if (!squareIsValid)
+                                        break;
+                                }
+
+                                // If valid, removes the square and updates the operations list
+                                if (squareIsValid)
+                                {
+                                    for (int y = squareOriginY; y < (squareOriginY + squareSize); y++)
+                                    {
+                                        for (int x = squareOriginX; x < (squareOriginX + squareSize); x++)
+                                        {
+                                            alreadyFilledPixels[y][x] = true;
+                                        }
+                                    }
+
+                                    operations.Add(new Fill()
+                                    {
+                                        x = squareOriginX,
+                                        y = squareOriginY,
+                                        size = squareSize
+                                    });
+                                }
+                            }
                         }
                     }
 
                     // OUTPUT
+                    System.Diagnostics.Debug.WriteLine("Writing output operations to file...");
+
                     using (StreamWriter outputFile = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), outputFileName)))
                     {
                         foreach (Operation operation in operations)
