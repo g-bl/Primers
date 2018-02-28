9dcba59265e93bea8065276eb17bc382966e2b8b
Guillaume Blaise <guillaume.blaise@mythalesgroup.com>
2018-02-28 11:25:13 +0100

    Illumination + AttrapezLesTous

--- AttrapezLesTous/AttrapezLesTous.cs
+++ AttrapezLesTous/AttrapezLesTous.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,10 +11,22 @@ namespace Primers
         public List<int> attacks;
     }
 
+    class Search
+    {
+        public List<Pokemon> availablePokemons;
+        public List<Pokemon> team;
+        public List<int> atksPool;
+    }
+
     class AttrapezLesTous
     {
         private static string inputFileName = "input_3.txt"; // http://primers.xyz/3
         private static char delimiter = ',';
+
+        private static List<Pokemon> pokemonsDb;
+        private static List<Search> searchs;
+
+        private static bool secondChoice;
 
         static void Main(string[] args)
         {
@@ -22,13 +35,13 @@ namespace Primers
             if (File.Exists(inputFilePath))
             {
                 // INPUT LOADING
-                List<Pokemon> pokemons = new List<Pokemon>();
+                pokemonsDb = new List<Pokemon>();
 
                 string[] lines = File.ReadAllLines(inputFilePath);
 
-                foreach (string line in lines)
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    string[] splittedLine = line.Split(delimiter);
+                    string[] splittedLine = lines[i].Split(delimiter);
 
                     if (splittedLine.Length > 1)
                     {
@@ -40,7 +53,7 @@ namespace Primers
                                 atks.Add(atk);
                         }
 
-                        pokemons.Add(new Pokemon()
+                        pokemonsDb.Add(new Pokemon()
                         {
                             name = splittedLine[0],
                             attacks = atks
@@ -48,33 +61,90 @@ namespace Primers
                     }
                 }
 
-                // SOLVING
-                List<Pokemon> team = new List<Pokemon>();
-                List<int> atksPool = new List<int>();
+                // SOLVING: Random-Multiple-DFS-Glouton-DoubleChoice
 
-                for (int i = 0; i < 6; i++)
+                searchs = new List<Search>();
+                secondChoice = false;
+
+                for (int i = 0; i < 10000; i++)
                 {
-                    // First, removes the last pokemon chosen in team from the available pokemons list.
-                    if (team.Count > 1)
-                        pokemons.Remove(team.Last<Pokemon>());
+                    List<Pokemon> pkms = new List<Pokemon>(pokemonsDb);
+                    Pokemon[] pkmsArray = pkms.ToArray();
 
-                    // Compares all the available pokemons based on the union of their attacks and the team's attacks.
-                    team.Add(pokemons.Aggregate((p1, p2) => (atksPool.Union(p1.attacks).ToList().Count > atksPool.Union(p2.attacks).ToList().Count) ? p1 : p2));
+                    Random rng = new Random();
+                    int nbPokemonsToBeRemoved = rng.Next(1, 10);
 
-                    // Finally, updates the union of chosen Pokemons' atks
-                    atksPool = atksPool.Union(team.Last<Pokemon>().attacks).ToList();
+                    for (int j = 0; j < nbPokemonsToBeRemoved; j++)
+                    {
+                        int pokemonId = rng.Next(0, pokemonsDb.Count);
+                        if (pokemonId < pkmsArray.Length && pkmsArray[pokemonId] != null)
+                        {
+                            pkms.Remove(pkmsArray[pokemonId]);
+                        }
+                    }
+
+                    ComputeSearch(pkms, new List<Pokemon>(), new List<int>());
                 }
 
                 // OUTPUT
+                System.Diagnostics.Debug.WriteLine("#searchs: " + searchs.Count);
+
+                Search bestSearch = searchs.Aggregate((s1, s2) => (s1.atksPool.Count > s2.atksPool.Count) ? s1 : s2);
+
                 System.Diagnostics.Debug.WriteLine("Best team:");
-                foreach (Pokemon pokemon in team)
+                foreach (Pokemon pokemon in bestSearch.team)
                 {
                     System.Diagnostics.Debug.WriteLine(pokemon.name);
                 }
-                System.Diagnostics.Debug.WriteLine("#atks: " + atksPool.Count);
+                System.Diagnostics.Debug.WriteLine("#atks: " + bestSearch.atksPool.Count);
             }
             else
                 System.Diagnostics.Debug.WriteLine("Error: missing file.");
+        }
+
+        static void ComputeSearch(List<Pokemon> availablePokemons, List<Pokemon> team, List<int> atksPool)
+        {
+            if (team.Count < 6)
+            {
+                // First, removes the last pokemon chosen in team from the available pokemons list.
+                if (team.Count > 1)
+                {
+                    availablePokemons.Remove(team.Last());
+                }
+
+                // Compares all the available pokemons based on the union of their attacks and the team's attacks.
+                // First choice,
+                Pokemon firstPokemon = availablePokemons.Aggregate((p1, p2) => (atksPool.Union(p1.attacks).ToList().Count > atksPool.Union(p2.attacks).ToList().Count) ? p1 : p2);
+
+                List<Pokemon> firstTeam = new List<Pokemon>(team);
+                firstTeam.Add(firstPokemon);
+                List<int> firstAtksPool = atksPool.Union(firstTeam.Last().attacks).ToList();
+
+                ComputeSearch(availablePokemons, firstTeam, firstAtksPool);
+
+                // and second choice.
+                if (secondChoice)
+                {
+                    availablePokemons.Remove(firstPokemon);
+                    Pokemon secondPokemon = availablePokemons.Aggregate((p1, p2) => (atksPool.Union(p1.attacks).ToList().Count > atksPool.Union(p2.attacks).ToList().Count) ? p1 : p2);
+                    availablePokemons.Add(firstPokemon);
+
+                    List<Pokemon> secondTeam = new List<Pokemon>(team);
+                    secondTeam.Add(secondPokemon);
+                    List<int> secondAtksPool = atksPool.Union(secondTeam.Last().attacks).ToList();
+
+                    ComputeSearch(availablePokemons, secondTeam, secondAtksPool);
+                }
+            }
+            else
+            {
+                searchs.Add(new Search()
+                {
+                    availablePokemons = availablePokemons,
+                    team = team,
+                    atksPool = atksPool
+                });
+            }
         }
     }
 }
---
+++ Illumination/Illumination.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primers
+{
+    class Illumination
+    {
+        private static string inputFileName = "input_6.txt"; // http://primers.xyz/3
+        private static char delimiter = ',';
+
+        public class Interrupteur
+        {
+            public int id;
+            public List<int> ampoules;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), inputFileName);
+
+            if (File.Exists(inputFilePath))
+            {
+                // INPUT LOADING
+                int nbAmpoules = -1;
+                int nbInterrupteurs = -1;
+                List<Interrupteur> interrupteurs = new List<Interrupteur>();
+                bool[] ampoules;
+
+                string[] lines = File.ReadAllLines(inputFilePath);
+
+                string[] firstLine = lines[0].Split(delimiter);
+                int.TryParse(firstLine[0], out nbAmpoules);
+                int.TryParse(firstLine[1], out nbInterrupteurs);
+
+                ampoules = new bool[nbAmpoules];
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] splittedLine = lines[i].Split(delimiter);
+
+                    if (splittedLine.Length > 1)
+                    {
+                        List<int> amps = new List<int>();
+                        for (int j = 1; j < splittedLine.Length; j++)
+                        {
+                            int ampID = -1;
+                            if (int.TryParse(splittedLine[j], out ampID))
+                                amps.Add(ampID);
+                        }
+
+                        Interrupteur interrupteur = new Interrupteur()
+                        {
+                            id = (i - 1),
+                            ampoules = amps
+                        };
+                        interrupteurs.Add(interrupteur);
+                    }
+
+                    //ampoules[i - 1] = false; // Les ampoules sont toutes off
+                }
+
+                // SOLVING
+                List<Interrupteur> finalInterrupteurs = new List<Interrupteur>();
+
+                while (interrupteurs.Count > 0)
+                {
+                    Interrupteur nextInterrupteur = interrupteurs.Aggregate((i1, i2) => (FutureIllumination(i1, ampoules) > FutureIllumination(i2, ampoules)) ? i1 : i2);
+                    finalInterrupteurs.Add(nextInterrupteur);
+
+                    interrupteurs.Remove(nextInterrupteur);
+                    foreach (int amp in nextInterrupteur.ampoules)
+                    {
+                        ampoules[amp] = true;
+                    }
+                }
+
+                // OUTPUT
+                foreach (Interrupteur interrupteur in finalInterrupteurs)
+                {
+                    System.Diagnostics.Debug.WriteLine(interrupteur.id);
+                }
+                System.Diagnostics.Debug.WriteLine("#interrupteur: " + finalInterrupteurs.Count);
+            }
+            else
+                System.Diagnostics.Debug.WriteLine("Error: missing file.");
+        }
+
+        static int FutureIllumination(Interrupteur interrupteur, bool[] ampoules)
+        {
+            bool[] potentialAmpoules = ampoules;
+            foreach (int amp in interrupteur.ampoules)
+            {
+                potentialAmpoules[amp] = true;
+            }
+            int total = 0;
+            foreach (bool on in potentialAmpoules)
+            {
+                if (on) total++;
+            }
+            return total;
+        }
+    }
+}
